565a3f25c62e523e2f16b5be4ee6312d0cd9c188
rachid ouhammou <rachidouhammou21@gmail.com>
2023-02-26 23:21:11 +0100

    finishing the side bare .

--- app/src/main/java/com/example/appbaby/controllers/activities/MainActivity.java
+++ app/src/main/java/com/example/appbaby/controllers/activities/MainActivity.java
@@ -1,16 +1,67 @@
 package com.example.appbaby.controllers.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 import com.example.appbaby.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // configuration:
+        configureToolBar();
+        configureDrawerLayout();
+        configureNavigationView();
     }
+    private void configureNavigationView() {
+        mNavigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void configureDrawerLayout() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    private void configureToolBar() {
+        mToolbar = (Toolbar) findViewById(R.id.activity_main_tool_bar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
 }
